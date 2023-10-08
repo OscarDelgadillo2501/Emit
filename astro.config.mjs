669c4b398feaeeaecb5f04e29c2fb3d828178d07
OscarDelgadillo2501 <thelorito@gmail.com>
2023-10-08 10:49:06 -0400

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import tailwind from "@astrojs/tailwind"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-})
+  site: "https://oscardelgadillo2501.github.io"
+});
